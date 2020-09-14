50d61586d78f83429e595fa36239864cb48f22d1
Kenji <kenji.au9@gmail.com>
2020-09-14 15:07:02 -0600

    Add the remove To Do action

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -10,19 +10,28 @@
  * considered "CONSTANTS")
  */
 
- // Think carefully for each action...
- // What parameter ( | data) do we need to pass it for it to complete
- // its job???
+// Think carefully for each action...
+// What parameter ( | data) do we need to pass it for it to complete
+// its job???
 
- const addNewToDo = toDoContent => {
-   return {
-     // This is the NAME of our action.
-     // "type" is how our 'reducer' knows what to do.
-     type: 'ADD_NEW_TO_DO',
-     // This is the DATA we are passing through to the reducer
-     // so that IT can do ITS JOB!
-     payload: toDoContent
-   }
- }
+const addNewToDo = toDoContent => {
+  return {
+    // This is the NAME of our action.
+    // "type" is how our 'reducer' knows what to do.
+    type: 'ADD_NEW_TO_DO',
+    // This is the DATA we are passing through to the reducer
+    // so that IT can do ITS JOB!
+    payload: toDoContent
+  }
+}
+// Another action: remove a to-do!
+// This time we don't need content - the To-Do already exists!
+// We DO need to target it shomehow though... so an ID will do the trick.
+const removeToDo = id => {
+  return {
+    type: 'REMOVE_TO_DO',
+    payload: id
+  };
+};
 
- export { addNewToDo };
+export { addNewToDo };
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 /** 
  * Redux Reducer(s)
  * A reducer will actually carry out the request or
@@ -5,33 +6,43 @@
  * passed that matches a type programmed-in.
  */
 
- // Typically we'll see two paramters:
- // 1. State (default value)
- // 2. action (the request being sent)
- const toDosReducer = (state = [], action ) => {
-   // Which action is this? What should we do?
-  switch ( action.type ) {
+// Typically we'll see two paramters:
+// 1. State (default value)
+// 2. action (the request being sent)
+const toDosReducer = (state = [], action) => {
+  // Which action is this? What should we do?
+  switch (action.type) {
     // If this is the ADD NEW Action
     case ('ADD_NEW_TO_DO'):
-    {
-      // Prepare a new task object
-      const newTask = {
-        task: action.payload
+      {
+        // Prepare a new task object
+        const newTask = {
+          id: uuidv4(),
+          task: action.payload
+        }
+        // Create a COPY of the original state array
+        const newToDoList = [...state];
+        newToDoList.push(newTask);
+        // Return the updated state (overwrites the state.)
+        // The return will overwrite the previous state passed into the reducer!
+        return newToDoList;
       }
-      // Create a COPY of the original state array
-      const newToDoList = [...state];
-      newToDoList.push(newTask);
-      // Return the updated state (overwrites the state.)
-      // The return will overwrite the previous state passed into the reducer!
-      return newToDoList;
-    }
-    // By default, make no change.
+    case ('REMOVE_TO_DO'):
+      {
+        const updateToDoList = state.filter(
+          // The array will be composed only ofitems that do not have the ID we passed.
+          toDo => toDo.id != action.payload 
+        );
+        // Return / overwrite the state with this new array of To-Dos
+        return updateToDoList;
+      }
+    // By default, make no change. (if the action type doesn't match)
     default:
-    {
-      return state;
-    }
-    
-  };
- };
+      {
+        return state;
+      }
 
- export default toDosReducer;
+  };
+};
+
+export default toDosReducer;
