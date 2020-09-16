679dcca1835db0919c861b77f84ab586535a281a
Kenji <kenji.au9@gmail.com>
2020-09-16 13:34:39 -0600

    Added in Nav Component

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Nav.css';
 
 function Nav() {
 
   return (
-    <nav>
+    <nav className="to-do-nav">
       <h2>To-Do App Navigation</h2>
       <ul>
         <li>
